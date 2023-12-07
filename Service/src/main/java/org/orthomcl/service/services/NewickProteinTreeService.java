8577c43fb075209bf186338f652d5bfd0d51ac51
Danielle Callan <dcallan@upenn.edu>
2023-12-07 12:26:39 -0500

    readability and error handling

--- Service/src/main/java/org/orthomcl/service/services/NewickProteinTreeService.java
+++ Service/src/main/java/org/orthomcl/service/services/NewickProteinTreeService.java
@@ -1,5 +1,9 @@
 package org.orthomcl.service.services;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,14 +24,18 @@ public class NewickProteinTreeService extends AbstractWdkService {
         String projectId = getWdkModel().getProjectId();
         String buildNumber = getWdkModel().getBuildNumber();
         String webservicesDir = getWdkModel().getProperties().get("WEBSERVICEMIRROR");
-        // TODO need to add orthoGroupId to this path, update to reflect actual webservice mirror
+        // TODO need to add orthoGroupId to this path, update to reflect actual webservice mirror directory structure
         // this should be considered a placeholder really
         String newickPath = webservicesDir + "/" + projectId + "/" + "build-" + buildNumber + "/data/newick";
 
-        try (Stream<String> newick = 
-            Files.lines(Files.newBufferedReader(Paths.get(newickPath), StandardCharsets.UTF_8)
-                .lines())) {
-             lines.forEach(System.out::println); //debug
+        try (BufferedReader br = new BufferedReader(new FileReader(newickPath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
 
         JSONObject jsonObject = new JSONObject();
