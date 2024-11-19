974e4eafab8db87d7ea0c35577b41ddf9c6147e9
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-11-19 10:20:44 -0500

    Move multi-blast guest-to-user job transfer to EBRC and enable in OrthoMCL

---
+++ Service/src/main/java/org/orthomcl/service/services/OrthoSessionService.java
@@ -0,0 +1,20 @@
+package org.orthomcl.service.services;
+
+import org.eupathdb.common.model.MultiBlastServiceUtil;
+import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.user.User;
+import org.gusdb.wdk.service.service.SessionService;
+
+public class OrthoSessionService extends SessionService {
+
+  @Override
+  protected void transferOwnership(User oldUser, User newUser, WdkModel wdkModel) throws WdkModelException {
+
+    // transfer strategies and datasets
+    super.transferOwnership(oldUser, newUser, wdkModel);
+
+    // also transfer multi-blast jobs
+    MultiBlastServiceUtil.transferMultiBlastJobs(oldUser, newUser, wdkModel, this);
+  }
+}
